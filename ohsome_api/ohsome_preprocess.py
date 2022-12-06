fca383d65988a52c6691f81a23bf1b9acaf1c5ef
Leah Rothschild <leahisrothschild@gmail.com>
2022-12-06 03:20:14 +0100

    Changes made to the functions for clarity, there are now 4 methods, the first is for overall landuse data without the mapping (get_overall_landuse_data), the second is for getting the specific data for mapping (get_data_for_map), the third cleans the data and puts landuse in categories(still to finish) the last one produces the map(still to finish)

--- ohsome_api/ohsome_preprocess.py
+++ ohsome_api/ohsome_preprocess.py
@@ -4,7 +4,7 @@ from ohsome import OhsomeClient
 import matplotlib.pyplot as plt
 
 #https://wiki.openstreetmap.org/wiki/Key:landuse
-def get_data(bbox:list, time:str, filter:str = 'landuse=*') -> pd.DataFrame:
+def get_overall_landuse_data(bbox:list, time:str, filter:str = 'landuse=*') -> pd.DataFrame:
     '''
     Returns a stacked pd DataFrame with the types of landuse for multiple bboxes.
     The inputs are:
@@ -18,7 +18,7 @@ def get_data(bbox:list, time:str, filter:str = 'landuse=*') -> pd.DataFrame:
     return response.as_dataframe()
 
 
-def get_map(bbox:list, time:str, filter:str):
+def get_data_for_map(bbox:list, time:str, filter:str):
     '''
     Returns both the Geopandas dataframe (response_df) and the map of an area based on the landuse
     The inputs are:
@@ -32,3 +32,15 @@ def get_map(bbox:list, time:str, filter:str):
     response_df = response.as_dataframe()
     fig, ax = plt.subplots(1,1, figsize = (20,20))
     return response_df, response_df.plot(column = 'landuse', ax = ax, legend= True)
+
+
+def clean_data_for_map(df):
+    '''
+    The input for this method is the dataframe from the get_map_and_data function and returns a cleaned DataFrame
+    All of the columns a part from 'geometry' and 'landuse' will be dropped as the levels of NaNs are very high (min is 87&)
+    '''
+    return df[['geometry', 'landuse']]
+
+def get_map(df):
+    fig, ax = plt.subplots(1,1, figsize = (20,20))
+    return df.plot(column = 'landuse', ax=ax, legend = True)
