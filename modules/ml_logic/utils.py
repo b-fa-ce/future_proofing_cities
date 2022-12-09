37dca1439b17629b26a637f11a728a3a6767d9f2
Bruno Faigle-Cedzich <faigle-cedzich@posteo.de>
2022-12-09 15:07:18 +0100

    utils: function which returns corrct const number of pixels per tile

--- modules/ml_logic/utils.py
+++ modules/ml_logic/utils.py
@@ -1,6 +1,12 @@
 import numpy as np
 import pandas as pd
 from ast import literal_eval
+import os
+
+from modules.data_aggregation.params import CITY_BOUNDING_BOXES
+
+# processed data
+INPUT_PATH = os.path.join('..','..','data','processed_data')
 
 
 def slice_picture_coords(full_coords: list, scaling_factor: int, overlap_percent: int = 0) -> list:
@@ -42,6 +48,61 @@ def slice_picture_coords(full_coords: list, scaling_factor: int, overlap_percent
     return tiles_coords
 
 
+def get_all_sub_coords(full_df: pd.DataFrame, num_px_lon = 10, num_px_lat = 6) -> pd.DataFrame:
+    """
+    description:
+    returns the sub image according to the smaller
+    tile coordinates
+
+    input:
+    full_df: full original dataframe
+    tiles_coords: coordinates of all subtiles from slice_picture_coords()
+    image_number: number of image to return
+
+    output:
+    returns the image_number-th subtile dataframe
+    """
+
+    # drop irrelavant features
+    full_df.drop(columns=['LST', 'ele','ll_corner', 'ur_corner', 'bb'])
+
+
+    # convert imported upper left corner str into list
+    ul_import = full_df.ul_corner.apply(literal_eval)
+    lr_import = full_df.lr_corner.apply(literal_eval)
+
+    # divide ul_corner into lists of lat and lon
+    ul_lat = np.array([ul[0] for ul in ul_import])[:,0]
+    ul_lon = np.array([ul[0] for ul in ul_import])[:,1]
+
+    # divide lr_corner into lists of lat and lon
+    lr_lat = np.array([lr[0] for lr in lr_import])[:,0]
+    lr_lon = np.array([lr[0] for lr in lr_import])[:,1]
+
+    total_num_tiles = int(len(full_df)/(num_px_lon* num_px_lat))
+
+    tiles_coords = []
+
+    for i in range(total_num_tiles):
+
+        # get pixel size
+        lat_size, lon_size = lr_lat[i] - ul_lat[i], lr_lon[i] - ul_lon[i]
+
+        # step size
+        step_lat = num_px_lat * lat_size
+        step_lon = num_px_lon * lon_size
+
+
+        # divide slice_coords into lists of lat and lon
+        slice_bound_lat = [ul_lat[i], ul_lat[i] + step_lat]
+        slice_bound_lon = [ul_lon[i], ul_lon[i] + step_lon]
+
+        tiles_coords.append([slice_bound_lat, slice_bound_lon])
+
+
+    return tiles_coords
+
+
 def get_sub_tile(full_df: pd.DataFrame, tiles_coords: list, image_number: int) -> pd.DataFrame:
     """
     description:
@@ -75,7 +136,7 @@ def get_sub_tile(full_df: pd.DataFrame, tiles_coords: list, image_number: int) -
     return sub_df
 
 
-# Compute all possible permutations
+
 def get_permutations(ndim_list):
     """
     returns all permutations of sublist elements
@@ -108,3 +169,14 @@ def get_corners(slice_coords: list):
     """
 
     return [get_correct_odering(get_permutations(coords)) for coords in slice_coords]
+
+
+if __name__ == '__main__':
+    city = 'Paris'
+    df_path = os.path.join(INPUT_PATH, city, f'{city}.csv')
+    coords_path = os.path.join(INPUT_PATH, city, f'{city}.npy')
+
+    df = pd.read_csv(df_path)
+    coords = get_all_sub_coords(df)
+
+    np.save(coords_path, coords)
